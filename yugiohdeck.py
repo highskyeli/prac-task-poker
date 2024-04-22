12c59d1d119607802f1b735ca5f71c15486f5251
eli descallar <elidescallar01@gmail.com>
2024-04-22 21:32:28 +0000

    yugiohdeck

--- yugiohdeck.py
+++ yugiohdeck.py
@@ -12,29 +12,24 @@ def createAcard(name, typec, rarity, attribute, level):
     card["name"] = name  
     card["typec"] = typec
     card["rarity"] = rarity
-    card["attribute"] = attribute 
     card["level"] = level
-
-    if card["typec"] not in ["Link", "Effect", "Spell", "XYZ", "Dragon"]:
-        type = input("Inserted Wrong Card Type, Please put it in one of these selections: Link, Effect, Spell, XYZ, and Dragon.")
-    deck.append(card)
+    card["attribute"] = attribute 
+    
 
     if card["attribute"] not in ["Dark", "Light", "Fire", "Water", "Wind", "Earth"]:
-        attribute = input("Inserted Wrong Card Attribute, Please put it in one fo these selections: Dark, Light, Fire, Water, Wind, Earth")
+        attribute = input("Inserted Wrong Card Attribute, Please put it in one of these selections: Dark, Light, Fire, Water, Wind, Earth ")
     deck.append(card)
-
-    if card["rarity"] not in ["Common", "Silver", "Gold"]:
-        rarity = input("Inserted Wrong Card Attribute, Please put it in one fo these selections: Dark, Light, Fire, Water, Wind, Earth")
-    deck.append(card)
-
+    
+    
 for i in range(5): 
     
 
     name = input("What is your card's name? ")
     typec = input("What is type of card would you like to have? ")
     rarity = input("what is your card's rarity? ")
-    attribute = input("What is your card's attribute?")
     level = input("What level is your card?")
+    attribute = input("What is your card's attribute?")
+    
 
     createAcard(name, typec, rarity, attribute, level)
     print(deck) 
