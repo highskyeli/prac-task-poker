a2296ede5c1eed237700f23a8728539a963c061d
eli descallar <elidescallar01@gmail.com>
2024-04-24 21:07:16 +0000

    yugiohnew

--- yugiohdeck.py
+++ yugiohdeck.py
@@ -5,6 +5,27 @@ deck = []
 
 def createAcard(name, typec, rarity, attribute, level):
  
+    tries = 5
+
+    while rarity not in ["Common", "Rare", "Ultra-Rare"]:
+        if tries <= 0:
+            print("Wow. It's taken you more than 4 tries to get it right. Wow.")
+
+        rarity = input("Inserted Wrong Card Rarity, Please put it in one of these selections: Common, Rare, Ultra-Rare ")
+        tries -= 1
+        
+    tries = 5
+
+
+    while attribute not in ["Dark", "Light", "Fire", "Water", "Wind", "Earth"]:
+        if tries <= 0:
+            print("Wow. It's taken you more than 4 tries to get it right. Wow.")        
+        attribute = input("Inserted Wrong Card Attribute, Please put it in one of these selections: Dark, Light, Fire, Water, Wind, Earth ")
+        tries -= 1
+
+
+
+
     card = {
 
     }
@@ -26,27 +47,8 @@ for i in range(5):
     name = input("What is your card's name? ")
     typec = input("What is type of card would you like to have? ")
     rarity = input("what is your card's rarity? ")
-
-    tries = 5
-
-    while rarity not in ["Common", "Rare", "Ultra-Rare"]:
-        if tries <= 0:
-            print("Wow. It's taken you more than 4 tries to get it right. Wow.")
-
-        rarity = input("Inserted Wrong Card Rarity, Please put it in one of these selections: Common, Rare, Ultra-Rare ")
-        tries -= 1
-        
-    tries = 5
-
     level = input("What level is your card?")
-
     attribute = input("What is your card's attribute?")
-
-    while attribute not in ["Dark", "Light", "Fire", "Water", "Wind", "Earth"]:
-        if tries <= 0:
-            print("Wow. It's taken you more than 4 tries to get it right. Wow.")        
-        attribute = input("Inserted Wrong Card Attribute, Please put it in one of these selections: Dark, Light, Fire, Water, Wind, Earth ")
-        tries -= 1
    
 
 
