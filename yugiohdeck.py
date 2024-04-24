f9a09714deaab0cc1bc721219784d67580fe86eb
eli descallar <elidescallar01@gmail.com>
2024-04-24 21:31:50 +0000

    yugiohdeckfixed2.0

--- yugiohdeck.py
+++ yugiohdeck.py
@@ -7,7 +7,7 @@ def createAcard(name, typec, rarity, attribute, level):
  
     tries = 5
 
-    while rarity not in ["Common", "Rare", "Ultra-Rare"]:
+    while rarity not in ["common", "rare", "ultra-rare"]:
         if tries <= 0:
             print("Wow. It's taken you more than 4 tries to get it right. Wow.")
 
@@ -17,7 +17,7 @@ def createAcard(name, typec, rarity, attribute, level):
     tries = 5
 
 
-    while attribute not in ["Dark", "Light", "Fire", "Water", "Wind", "Earth"]:
+    while attribute not in ["dark", "light", "fire", "water", "wind", "earth"]:
         if tries <= 0:
             print("Wow. It's taken you more than 4 tries to get it right. Wow.")        
         attribute = input("Inserted Wrong Card Attribute, Please put it in one of these selections: Dark, Light, Fire, Water, Wind, Earth ")
@@ -45,10 +45,10 @@ for i in range(5):
     
 
     name = input("What is your card's name? ")
-    typec = input("What is type of card would you like to have? ")
-    rarity = input("what is your card's rarity? ")
-    level = input("What level is your card?")
-    attribute = input("What is your card's attribute?")
+    typec = input("What is type of card would you like to have? An example could be Dragon, Plant, Aqua, Rock; it could be up to your imagination ")
+    rarity = input("what is your card's rarity? Choose from Common, Rare, Ultra-Rare ").lower()
+    level = input("What level is your card? ")
+    attribute = input("What is your card's attribute? Choose from Dark, Light, Fire, Water, Wind, Earth ").lower()
    
 
 
